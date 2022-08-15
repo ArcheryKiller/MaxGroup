b34842b5bef5f97ebf55164e7bc3c6bf498b5ed6
KilliLogan <ivanov-p-a@bk.ru>
2022-08-15 12:06:20 +0300

    create and settings AuthorizationFragment.kt

--- app/src/main/java/pavel/ivanov/maxgroup/MainActivity.kt
+++ app/src/main/java/pavel/ivanov/maxgroup/MainActivity.kt
@@ -2,10 +2,21 @@ package pavel.ivanov.maxgroup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pavel.ivanov.maxgroup.databinding.ActivityMainBinding
+import pavel.ivanov.maxgroup.ui.AuthorizationFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, AuthorizationFragment())
+                .commit()
+        }
     }
 }
